fcf448fca9bd08319c74fe30bf40e3b3d84f063e
Shay Rosner <shaymrosner@gmail.com>
2021-03-19 23:02:04 -0700

    add inital controller methods, not functional at the moment

---
+++ controller/controller.js
@@ -0,0 +1,29 @@
+const db = require('../database/index.js');
+
+module.exports.getAllProducts = (req, res) => {
+    db.queryAllProducts()
+      .then(response => {
+        res.json(response)
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: error,
+        })
+      })
+}
+
+module.exports.getOneProduct = (req, res) {
+  db.getOneProduct(req.params.id)
+    .then(response => {
+      if (response) {
+        res.json(response)
+      } else {
+        res.status(404).send('Product Not Found');
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: error,
+      })
+    })
+});
