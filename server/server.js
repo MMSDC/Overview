176049d7846bd52abd20c5be3eff928f010c1298
Shay Rosner <shaymrosner@gmail.com>
2021-03-21 10:31:02 -0700

    add controller routes

--- server/server.js
+++ server/server.js
@@ -9,19 +9,17 @@ const port = 3300;
 app.use(express.json());
 // GET /products
 app.get('/products', productController.getAllProducts)
+
 // GET /products/:product_id
-app.get('/products/:id', (req, res) => {
-  res.send('hi')
-})
+app.get('/products/:id', productController.getOneProduct)
+
 // GET /products/:product_id/styles
-app.get('/products/:id/styles', (req, res) => {
-  res.send('hi')
-})
+app.get('/products/:id/styles', productController.getProductStyles)
 
 // GET /products/:product_id/related
-app.get('/products/:id/related', (req, res) => {
-  res.send('hi')
-})
+// app.get('/products/:id/related', (req, res) => {
+//   res.send('hi')
+// })
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
